2de14fcb52b9724f884ff63deadebb79fefcd16d
Alistair-123 <alixtairloberternos@gmail.com>
2024-12-31 15:30:38 +0800

    Fixed functions syntax in netlify.toml

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // This is the folder to be published
+  },
   server: {
     host: true, // Automatically uses your local IP address
     proxy: {
